8010656ae38750bb72a1eeab2642b030c47d676a
MphoMutlaneng <mutlanengangel@gmail.com>
2024-05-10 14:27:10 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Your name: ");
+        String name = input.next();
+
         System.out.println("Hello world!");
     }
+    //.out.println("Hello world!");
 }
